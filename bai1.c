a24f7095739642bd1ddc1b00e7f4ec3ffb92c6fa
MinhDuy2007 <mduyeditor@gmail.com>
2026-02-05 08:40:10 +0700

    bai1.c

--- bai1.c
+++ bai1.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
 #define SIZE 10
 typedef struct MA_TRAN{
-	int a[SIZE][SIZE];
+	float a[SIZE][SIZE];
 	int d,c;
 }MT;
 
@@ -8,6 +9,37 @@ void nhap(MT *a);
 void xuat(MT a);
 
 int main(void){
-	
+	MT a;
+	nhap(&a);
+	printf("\n");
+	xuat(a);
 	return 0;
 }
+
+void nhap(MT *a){
+	int i, j;
+	printf("nhap so dong: ");
+	scanf("%d", &a->d);
+	
+	printf("\nnhap so cot: ");
+	scanf("%d", &a->c);
+	
+	//nhap gia tri
+	for(i = 0; i < a->d; i++){ //dem dong
+	printf("\nnhap gia tri cho dong %d: ", i + 1);
+		for(j = 0; j < a->c;j++){
+			printf("\nnhap gia tri cho cot %d: ", j + 1);
+			scanf("%f", &a->a[i][j]);
+		}
+	}
+}
+void xuat(MT a){
+	int i, j;
+	for(i = 0; i < a.d;i++){
+		for(j = 0; j < a.c; j++){
+			printf("%.2f\t", a.a[i][j]);
+		}
+			printf("\n");
+	}
+}
+
